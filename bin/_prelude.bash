3433817f788455607dc0d12ae9724cbc2932791c
Chuck Adams <chaz@chaz.works>
2025-09-01 15:57:58 -0600

    use set -o nounset in prelude

--- bin/_prelude.bash
+++ bin/_prelude.bash
@@ -1,12 +1,6 @@
 #!/usr/bin/false
 # This file should be sourced, not run
 
-[[ -n $TRACE ]] && [[ $TRACE != 0 ]] && set -o xtrace
-
-set -o errexit
-set -o pipefail
-# set -o nounset    # too strict IMHO: it breaks the basic shell idiom of [[ -n $SOMEOPTION ]]
-
 function warn {
     echo "$@" >&2
 }
@@ -21,5 +15,13 @@ function RUN() {
   [[ -n $DRY_RUN ]] && [[ $DRY_RUN != 0 ]] && _run=echo
   $_run "$@"
 }
+
+####
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+[[ ${TRACE:-0} != 0 ]] && set -o xtrace
 
 cd $(dirname $0)/..
